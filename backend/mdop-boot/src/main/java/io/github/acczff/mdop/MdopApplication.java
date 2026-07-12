91826840f262db2a8b5bc9a881f46112680ca7c0
acczff <38657587+acczff@users.noreply.github.com>
2026-07-12 20:50:41 +0800

    feat(boot): 建立最小启动应用和健康检查

---
+++ backend/mdop-boot/src/main/java/io/github/acczff/mdop/MdopApplication.java
@@ -0,0 +1,12 @@
+package io.github.acczff.mdop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MdopApplication.class, args);
+    }
+}
